882b9812a326b70578e527a981f40d0e6a1b93bd
chungheeLee <chlee6247@gmail.com>
2021-05-21 17:41:15 +0900

    " Portfolio project "

---
+++ src/com/chlee/Cafe/Cafe.java
@@ -0,0 +1,12 @@
+package com.chlee.Cafe;
+
+public class Cafe {
+
+	public static void main(String[] args) {
+		CoffeeType coffee = CoffeeType.아메리카노;
+		System.out.println("Lee cafe에 오신걸 환영합니다");
+		System.out.println("CoffeeType.아메리카노");
+
+	}
+
+}
---
+++ src/com/chlee/Cafe/CoffeeType.java
@@ -0,0 +1,5 @@
+package com.chlee.Cafe;
+
+enum CoffeeType {
+	아메리카노, 카페라떼, 플랫화이트, 카페모카, 카푸치노
+}
