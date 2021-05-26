13b1629a67784701ee353816e86066e92f858a04
chungheeLee <chlee6247@gmail.com>
2021-05-26 19:39:50 +0900

    new added

--- src/com/chlee/Cafe/Cafe.java
+++ src/com/chlee/Cafe/Cafe.java
@@ -3,10 +3,17 @@ package com.chlee.Cafe;
 public class Cafe {
 
 	public static void main(String[] args) {
-		CoffeeType coffee = CoffeeType.아메리카노;
-		System.out.println("Lee cafe에 오신걸 환영합니다");
-		System.out.println("CoffeeType.아메리카노");
-
-	}
-
+		CoffeeType Coffee = CoffeeType.아메리카노;
+		System.out.println("Lee cafe에 오신걸 환영합니다.");
+		CoffeeType coffeeTypes[] = CoffeeType.values();
+		
+		String menu = String.join("/",
+				CoffeeType.카페라떼.toString(),
+				CoffeeType.플랫화이트.toString(),
+				CoffeeType.카페모카.toString()
+				);
+		
+		System.out.println(menu);
+		String[] memus = menu.split("/");
+}
 }
